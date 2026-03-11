7f69ceb8857f0e88341a377c2b3931b6362995d3
Renaud Bronchart <renaudbronchart@MacBookAir.home>
2026-03-11 17:27:49 +0100

    TASK-1 create user entity

---
+++ src/main/java/com/example/taskmanagementapi/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.taskmanagementapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
+}
